565744d06446b5dbb40a08f0527fbec9006267e1
nkshn <vovanika23413@gmail.com>
2020-09-10 13:12:02 +0300

    added dummy model and data

---
+++ src/data/dummy-data.js
@@ -0,0 +1,10 @@
+import Product from '../model/product';
+
+export const PRODUCTS_DATA = [
+  new Product(1, 'Колумбія', 'Description for Колумбія', 229.50, 'https://via.placeholder.com/150/92c952', 'Середній', 'Фрукти, Шоколад, Карамель', 'Слабка кислотність'),
+  new Product(2, 'Танзанія', 'Description for Танзанія', 175, 'https://via.placeholder.com/150/771796', 'Світлий', 'Шоколад, Яблоко, Сік', 'Яскрава гіркота'),
+  new Product(3, 'Індія', 'Description for Індія', 240, 'https://via.placeholder.com/150/24f355', 'Темний', 'Фрукти, Шоколад, Овочі', 'Тяжка гіркота'),
+  new Product(4, 'Чорний Бленд', 'Description for Чорний Бленд', 15, 'https://via.placeholder.com/150/d32776', 'Світлий', 'Вино, Хліб, Карамель', 'Яскрава кислотність'),
+  new Product(5, 'Бразилія', 'Description for Бразилія', 1200.30, 'https://via.placeholder.com/150/f66b97', 'Середній', 'Дорога, Бензин, Ягоди', 'Світла гіркота'),
+  new Product(6, 'Ефіопія', 'Description for Ефіопія', 5, 'https://via.placeholder.com/150/56a8c2', 'Темний', 'Сонце, Ліс, Вітер', 'Тяжка кислотність'),
+]
---
+++ src/model/product.js
@@ -0,0 +1,14 @@
+class Product {
+  constructor(id, title, description, price, image, fryingDegree, aroma, taste) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.image = image;
+    this.fryingDegree = fryingDegree;
+    this.aroma = aroma;
+    this.taste = taste;
+  }
+}
+
+export default Product;
